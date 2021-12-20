280758c5d15f080558e306f18a9c07a91eba6d4b
Roger Goma <roger.goma96@gmail.com>
2021-12-20 23:53:42 +0100

    Add graphql plugin

--- config/plugins.js
+++ config/plugins.js
@@ -13,4 +13,13 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  graphql: {
+    endpoint: "/graphql",
+    shadowCRUD: true,
+    playgroundAlways: true,
+    depthLimit: 100,
+    apolloServer: {
+      tracing: false,
+    },
+  },
 });
