3cb8acad437b1a142402f7b68a1bafbe94484e6d
Roger Goma <roger.goma96@gmail.com>
2021-12-24 13:03:22 +0100

    Add pg package

--- config/plugins.js
+++ config/plugins.js
@@ -13,13 +13,4 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  graphql: {
-    endpoint: "/graphql",
-    shadowCRUD: true,
-    playgroundAlways: true,
-    depthLimit: 100,
-    apolloServer: {
-      tracing: false,
-    },
-  },
 });
