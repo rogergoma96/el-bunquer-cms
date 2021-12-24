6846b5d284e2bd7358b2b6e44fa672e1559cbc45
Roger Goma <roger.goma96@gmail.com>
2021-12-24 14:01:02 +0100

    Enable graphql to production mode

---
+++ config/env/production/plugins.js
@@ -0,0 +1,25 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_API_KEY"),
+        api_secret: env("CLOUDINARY_API_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  graphql: {
+    endpoint: "/graphql",
+    shadowCRUD: true,
+    playgroundAlways: true,
+    depthLimit: 100,
+    apolloServer: {
+      tracing: false,
+    },
+  },
+});
